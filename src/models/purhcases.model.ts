3f2bc5a5e6abbfced3cb4ce93845455d7347302c
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-04-10 23:32:35 +0330

    Add -> Initial entities are created

---
+++ src/models/purhcases.model.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { UsersEntity } from "./users.model";
+import { PlansEntity } from "./plans.model";
+
+@Entity("users")
+export class PurhcasesEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+  @Column({ nullable: false })
+  pricePaid: string;
+  @Column({ nullable: false, unique: true })
+  trackCode: string;
+  @Column({ nullable: false, type: "datetime" })
+  purchaseDate: Date;
+  @Column({ nullable: false, enum: ["waiting", "paid", "cancelled"] })
+  status: string;
+  @CreateDateColumn()
+  created_At: Date;
+  @UpdateDateColumn()
+  update_At: Date;
+
+  @ManyToOne(() => UsersEntity, (user) => user.purhcases, { nullable: false })
+  @JoinColumn({ name: "userId" })
+  user: UsersEntity;
+  @ManyToOne(() => PlansEntity, (plan) => plan.purhcases, { nullable: false })
+  @JoinColumn({ name: "planId" })
+  plan: PlansEntity;
+}
