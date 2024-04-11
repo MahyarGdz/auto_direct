33f43bff8bee13f44e906e9dfa139f1ec4374d23
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-04-11 18:17:04 +0330

    update -> Initial test of controller and router

--- src/modules/auth/auth.contoller.ts
+++ src/modules/auth/auth.contoller.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from "express";
 import { UserService } from "../users/user.service";
 import { Logger } from "winston";
+import { AuthLoginDto } from "./dto/authLogin.dto";
 
 export class AuthController {
   constructor(
@@ -8,9 +9,19 @@ export class AuthController {
     private logger: Logger,
   ) {}
 
-  async test(_req: Request, res: Response) {
-    this.logger.info("calling test()");
-    const msg = await this.userService.test();
-    res.json({ msg });
+  //   async test(_req: Request, res: Response) {
+  //     this.logger.info("calling test()");
+  //     const msg = await this.userService.test();
+  //     res.json({ msg });
+  //   }
+
+  async LoginC( req: Request, res: Response,) {
+
+    this.logger.info("call LoginC()");
+
+    const authDto: AuthLoginDto = req.body
+
+     res.json(authDto)
+    // return authDto
   }
 }
