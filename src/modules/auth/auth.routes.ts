35d164c90896f76a9d6c2d1e9df04a13e4464756
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-04-11 18:17:04 +0330

    update -> Initial test of controller and router

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -4,6 +4,8 @@ import { AuthController } from "./auth.contoller";
 import { UserService } from "../users/user.service";
 import UserRepository from "../users/user.repository";
 import { UsersEntity } from "../../models";
+import { ValidatorMiddlewares } from "../../middlewares";
+import { AuthLoginDto } from "./dto/authLogin.dto";
 
 const router: Router = express.Router();
 
@@ -11,7 +13,8 @@ const userRepo = new UserRepository(UsersEntity);
 const userService = new UserService(userRepo);
 const authController = new AuthController(userService, logger);
 
-// router.post("/login");
-router.get("/test", asyncWrapper(authController.test.bind(authController)));
+router.post("/login", ValidatorMiddlewares(AuthLoginDto), asyncWrapper(authController.LoginC.bind(authController)));
+
+// router.get("/test", asyncWrapper(authController.test.bind(authController)));
 
 export default router;
