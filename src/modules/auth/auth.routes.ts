08ad1cbe27b12f1aaa8aa135f19e45e7434b984e
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-04-11 19:00:59 +0330

    Update -> `LoginS` method has been added And The otp code is stored in the cache with this key: `{phone}:Login:Otp` Add -> install `node-cache`

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -6,12 +6,16 @@ import UserRepository from "../users/user.repository";
 import { UsersEntity } from "../../models";
 import { ValidatorMiddlewares } from "../../middlewares";
 import { AuthLoginDto } from "./dto/authLogin.dto";
+import { AuhtService } from "./auth.service";
+import NodeCache from "node-cache";
 
 const router: Router = express.Router();
 
 const userRepo = new UserRepository(UsersEntity);
+const cache = new NodeCache
 const userService = new UserService(userRepo);
-const authController = new AuthController(userService, logger);
+const authService = new AuhtService(userService,cache)
+const authController = new AuthController(authService,logger);
 
 router.post("/login", ValidatorMiddlewares(AuthLoginDto), asyncWrapper(authController.LoginC.bind(authController)));
 
