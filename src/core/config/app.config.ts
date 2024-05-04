262cf1275f435adbee3334d66748b4ed8d0f4afb
MahyarGdz <Mahyar.goudarzi@outlook.com>
2024-05-04 14:47:45 +0330

    refactor(app): remove the app file and integrate with server ts

---
+++ src/core/config/app.config.ts
@@ -0,0 +1,32 @@
+import { IAppOptions } from "../../common";
+
+export const options: IAppOptions = {
+  cors: { origin: true, credentials: true, optionsSuccessStatus: 204 },
+  helmet: {
+    //   contentSecurityPolicy: {
+    //     defaultSrc: ["'self'"],
+    //     scriptSrc: ["'self'", "'unsafe-inline'"],
+    //     sandbox: ["allow-forms", "allow-scripts"],
+    //     objectSrc: ["'none'"],
+    //     upgradeInsecureRequests: true,
+    //   },
+    contentSecurityPolicy: false,
+    noSniff: true,
+    referrerPolicy: { policy: "no-referrer" },
+    xDnsPrefetchControl: false,
+    crossOriginOpenerPolicy: false,
+    crossOriginResourcePolicy: false,
+    xPermittedCrossDomainPolicies: false,
+    originAgentCluster: false,
+    xDownloadOptions: false,
+  },
+  rate: {
+    windowMs: 60 * 60 * 1000, // 1 hour
+    //   max: 2500,
+    message: "Too many requests from this IP, please try again after an hour",
+    //   windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 60, // Limit each IP to 50 requests per  5
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,71 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
 dotenv.config();
-import http from "http";
-import app from "./app";
+import express, { Application } from "express";
+import passport from "passport";
+import compression from "compression";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+import { options as AppOptions } from "./core/config/app.config";
+import { errorHandler, lastHandler, notFoundHandler, jwtStrategy } from "./core";
 
 import { appDataSrc, Logger } from "./core";
-const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 const logger = new Logger();
 async function bootStrap(): Promise<void> {
   try {
+    const app: Application = express();
+    //remove x-powered-by header from response
+    app.disable("x-powered-by");
+    //compress the response
+
+    app.use(compression());
+    /**
+     * configure body parser
+     */
+    app.use(express.json());
+    /**
+     * configure morgan for http logging
+     */
+    app.use(morgan("dev"));
+    /**
+     * configure helmet
+     */
+    app.use(helmet(AppOptions.helmet));
+    /**
+     * configure cors
+     */
+    app.use(cors(AppOptions.cors));
+    /**
+     * configure rate limit
+     */
+    app.use(`/api/`, rateLimit(AppOptions.rate));
+    /**
+     * configure passport
+     */
+    app.use(passport.initialize());
+    passport.use("jwt", jwtStrategy);
+    /**
+     * configure app routes
+     */
+    // app.use("/api/v1", router);
+    /**
+     * configure error handler
+     */
+    app.use(notFoundHandler);
+    app.use(errorHandler);
+    app.use(lastHandler);
+
+    /**
+     * connect database
+     **/
     await appDataSrc.initialize();
+
     logger.info("The database has been initialized.");
-    server.listen(port, () => {
+    app.listen(port, () => {
       logger.info(`server is starting on http://localhost:${port}`);
     });
   } catch (error) {
