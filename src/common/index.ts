d904b235367e6871124a12bb74a064bb534bc344
MahyarGdz <Mahyar.goudarzi@outlook.com>
2024-05-04 15:02:26 +0330

    chore(app): add ilogger to interfaces

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,4 +2,5 @@ export { AuthTokenPayload, Token } from "./types/jwtToken.type";
 export { IAppOptions } from "./interfaces/app/IAppOptions";
 export { IErrorResponse } from "./interfaces/response/IErrorResponse";
 export { ISmsResponse } from "./interfaces/response/IsmsResponse";
+export { ILogger } from "./interfaces/app/ILogger";
 export { AuthMessage, AuthMessageResponse } from "./enums/message.enum";
